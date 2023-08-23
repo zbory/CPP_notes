15c45387480e689f5a1804280620425e4c8368be
zbory <zborayb@gmail.com>
2023-08-23 19:49:51 +0200

    add Tag dispatch part

--- design_patterns/design_patterns.md
+++ design_patterns/design_patterns.md
@@ -1,10 +1,20 @@
 # Tervezési minták
 
 ## Tag dispatch
-Azonos szignatúrájú függvények szétválasztására. Azért hozunk létre osztályokat/struct-okat, hogy tag-ekként működjenek. Ugyanezt elérhetnénk enum-okkal, template-ekkel és futásidejű polimorfizmussal is.<br>
+The “tag” in tag dispatching refers to a type that has no behaviour and no data
 
-https://www.fluentcpp.com/2018/05/01/when-to-use-enums-and-when-to-use-tag-dispatching-in-cpp/#:~:text=Essentially%2C%20tags%20get%20dispatched%20at,dispatching%20relies%20on%20function%20overloading.<br>
+Azonos szignatúrájú függvények szétválasztására. Azért hozunk létre osztályokat/struct-okat, hogy tag-ekként működjenek. <br>(Ugyanezt elérhetnénk enum-okkal, template-ekkel és futásidejű polimorfizmussal is.)<br>
+
+Enum VS Tag dispatch: fő különbség
+- logika egy függvényben vagy több függvénybe szétválasztva
+- a feloldás ideje futási időben vagy fordítási időben
+
+
+
 [Example](hetfo/TagDispatch.cpp)
+
+* https://www.fluentcpp.com/2018/04/27/tag-dispatching/
+* https://www.fluentcpp.com/2018/05/01/when-to-use-enums-and-when-to-use-tag-dispatching-in-cpp/#:~:text=Essentially%2C%20tags%20get%20dispatched%20at,dispatching%20relies%20on%20function%20overloading.<br>
 ## Lérehozási minták
 ### Singleton
 [Example](hetfo/Singleton.cpp)
--- design_patterns/hetfo/TagDispatch.cpp
+++ design_patterns/hetfo/TagDispatch.cpp
@@ -1,4 +1,5 @@
-
+//////////////////////////////////////////////////////////////////////////
+// Training example
 
 struct Foo {
 
@@ -16,3 +17,63 @@ int main() {
 	auto f2 = Foo{ Foo::tag2 };
 }
 
+//////////////////////////////////////////////////////////////////////////
+// (Default) Constructors with Tag dispatch
+
+class MyClass
+{
+public:
+    static struct ConstructThisWay{} constructThisWay;
+    static struct ConstructThatWay{} constructThatWay;
+
+    explicit MyClass(ConstructThisWay);
+    explicit MyClass(ConstructThatWay);
+};
+
+
+MyClass x(MyClass::constructThatWay);
+
+//////////////////////////////////////////////////////////////////////////
+//              Tag Dispatch VS Enum
+
+// (1) Structure of the code
+// Tag Dispatch: separation of logic with same signature functions, good if the logic is quite different
+
+struct BehaveThisWay{};
+struct BehaveThatWay{};
+
+void f(int argument, BehaveThisWay);
+{
+    // use argument this way
+}
+
+void f(int argument, BehaveThatWay);
+{
+    // use argument that way
+}
+
+// Enum: logic in single function, good if the logic is mostly the same and then test the enum at the few junction points
+
+enum class BehaviourType
+{
+    thisWay,
+    thatWay
+};
+
+void f(int argument, BehaviourType behaviourType);
+{
+    // check the value of behaviourType and behave accordingly
+}
+
+// (2) Time of resolution
+// Tag Dispatch: compile time
+// Enum: runtime
+
+if (myBehaviour == BehaviourType::thisWay)
+{
+    f(value, BehaveThisWay());
+}
+else if (myBehaviour == BehaviourType::thatWay)
+{
+    f(value, BehaveThatWay());
+}
